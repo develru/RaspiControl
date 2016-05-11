70a291f63871b3bed7797679de6da9d0e4b11271
Richard Schwalk <richard.schwalk@gmail.com>
2016-05-11 21:20:06 +0200

    WeatherData defined

--- src/weather/weatherdata.cpp
+++ src/weather/weatherdata.cpp
@@ -1,6 +1,51 @@
 #include "weatherdata.h"
 
-WeatherData::WeatherData()
-{
+using namespace weather;
 
+WeatherData::WeatherData() :
+    m_isDataRecived(false),
+    m_locationName(""),
+    m_temperature(0),
+    m_description("")
+{
+}
+
+std::string WeatherData::locationName() const
+{
+    return m_locationName;
+}
+
+void WeatherData::setLocationName(const std::string &locationName)
+{
+    m_locationName = locationName;
+}
+
+int WeatherData::temperature() const
+{
+    return m_temperature;
+}
+
+void WeatherData::setTemperature(int temperature)
+{
+    m_temperature = temperature;
+}
+
+std::string WeatherData::description() const
+{
+    return m_description;
+}
+
+void WeatherData::setDescription(const std::string &description)
+{
+    m_description = description;
+}
+
+bool WeatherData::isDataRecived() const
+{
+    return m_isDataRecived;
+}
+
+void WeatherData::setIsDataRecived(bool isDataRecived)
+{
+    m_isDataRecived = isDataRecived;
 }
--- src/weather/weatherdata.h
+++ src/weather/weatherdata.h
@@ -1,11 +1,34 @@
 #ifndef WEATHERDATA_H
 #define WEATHERDATA_H
 
+#include <string>
+
+namespace weather {
 
 class WeatherData
 {
 public:
     WeatherData();
+
+    std::string locationName() const;
+    void setLocationName(const std::string &locationName);
+
+    int temperature() const;
+    void setTemperature(int temperature);
+
+    std::string description() const;
+    void setDescription(const std::string &description);
+
+    bool isDataRecived() const;
+    void setIsDataRecived(bool isDataRecived);
+
+private:
+    bool m_isDataRecived;
+    std::string m_locationName;
+    int m_temperature;
+    std::string m_description;
 };
+
+}
 
 #endif // WEATHERDATA_H
